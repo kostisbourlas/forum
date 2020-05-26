92a005b3299306778fded71c0f2b42c23f4ace01
Kostis Bourlas <kostisbourlas@protonmail.com>
2020-05-26 12:50:57 +0300

    add Channel in New Thread View and Validation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,10 @@
                             <a href="/threads">All Threads</a>
                         </li>
 
+                        <li>
+                            <a href="/threads/create">New Thread</a>
+                        </li>
+
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Channels
